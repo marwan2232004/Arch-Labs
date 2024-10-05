fdd190353467ff1692b5646c024c20958350389d
Youssef Tarek <myoussef9366@gmail.com>
2024-10-05 17:23:43 +0300

    feat: add ALU

--- Lab1/Behavioral/part_b.vhd
+++ Lab1/Behavioral/part_b.vhd
@@ -1,7 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-architecture rtl_b of Part is
+architecture behavioral_b of Part is
 	signal A_xor_B : std_logic_vector(7 downto 0);
 	signal A_nand_B : std_logic_vector(7 downto 0);
 	signal A_or_B : std_logic_vector(7 downto 0);
@@ -12,8 +12,13 @@ begin
 	A_nand_B <= A NAND B;
 	A_or_B <= A OR B;
 	not_A <= NOT A;
+	cout <= "0";
 	
-	iMux2 : entity work.Mux2(rtl) port map(
+	iMux2 : entity work.Mux2(rtl) 
+	generic map(
+        size => 8
+    )
+	port map(
 		A_xor_B,
 		A_nand_B,
 		A_or_B,
